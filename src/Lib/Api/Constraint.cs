64d850ce0fa545d993a61cb02281d501297af7f0
GreemDev <greemdev@ryujinx.app>
2025-07-19 00:05:08 -0500

    Change constrants into instances of base Constraint instead of polymorphic type

--- src/Lib/Api/Constraint.cs
+++ src/Lib/Api/Constraint.cs
@@ -2,7 +2,7 @@
 
 namespace Starscript;
 
-public abstract class Constraint
+public class Constraint
 {
     public Predicate<int> Test { get; }
     public Func<string> FriendlyError { get; }
@@ -12,34 +12,28 @@ public abstract class Constraint
         Test = test;
         FriendlyError = friendlyError;
     }
-
-    public static readonly Constraint None = new NoConstraint();
-    public static Constraint ExactCount(int count) => new ExactCountConstraint(count);
-    public static Constraint AtLeast(int min) => new AtLeastConstraint(min);
-    public static Constraint AtMost(int max) => new AtMostConstraint(max);
-    public static Constraint Within(int min, int max) => new WithinConstraint(min, max);
-
+    
     public string GetError(StarscriptFunctionContext context)
         => $"{context.FormattedName} requires {FriendlyError()}, got {context.ArgCount}.";
 
-    private sealed class NoConstraint() : Constraint(_ => true, () => string.Empty);
+    public static readonly Constraint None = new(_ => true, () => string.Empty);
 
-    private sealed class ExactCountConstraint(int count) : Constraint(
+    public static Constraint ExactCount(int count) => new(
         it => it == count,
         () => "argument".Pluralize(count, prefixQuantity: true)
     );
-
-    private sealed class AtLeastConstraint(int min) : Constraint(
+    
+    public static Constraint AtLeast(int min) => new(
         it => it >= min,
         () => $"at least {"argument".Pluralize(min, prefixQuantity: true)}"
     );
 
-    private sealed class AtMostConstraint(int min) : Constraint(
-        it => it <= min,
-        () => $"at most {"argument".Pluralize(min, prefixQuantity: true)}"
+    public static Constraint AtMost(int max) => new(
+        it => it <= max,
+        () => $"at most {"argument".Pluralize(max, prefixQuantity: true)}"
     );
-
-    private sealed class WithinConstraint(int min, int max) : Constraint(
+    
+    public static Constraint Within(int min, int max) => new(
         it => it >= min && it <= max,
         () => $"{min}..{max} {"argument".Pluralize(min + max)}"
     );
