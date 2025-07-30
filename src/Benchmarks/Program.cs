1fde56a0d30b5a1693d0be42aec3caf8033f6b17
GreemDev <greemdev@ryujinx.app>
2025-07-29 23:45:22 -0500

    benchmark project

---
+++ src/Benchmarks/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+using Starscript.Net.Benchmarks;
+
+BenchmarkRunner.Run<StarscriptVsStringFormat>();
---
+++ src/Benchmarks/StarscriptVsStringFormat.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Starscript.Net.Benchmarks;
+
+public class StarscriptVsStringFormat
+{
+    public static StarscriptHypervisor Hypervisor { get; } = StarscriptHypervisor.Create().WithStandardLibrary();
+
+    public const string FormatSource = "FPS: {0}";
+    public const string StarscriptSource = "FPS: {round(fps)}";
+
+    public static Script PrecompiledTestScript = Compiler.DirectCompile(StarscriptSource);
+
+    public StarscriptVsStringFormat()
+    {
+        Hypervisor.Set("fps", 59.68223);
+    }
+
+    [Benchmark]
+    public void StringFormat()
+    {
+        _ = string.Format(FormatSource, Math.Round(59.68223));
+    }
+    
+    [Benchmark]
+    public void PrecompiledStarscript()
+    {
+        _ = PrecompiledTestScript.Execute(Hypervisor);
+    }
+    
+    [Benchmark]
+    public void JitStarscript()
+    {
+        _ = Compiler.DirectCompile(StarscriptSource).Execute(Hypervisor);
+    }
+}
